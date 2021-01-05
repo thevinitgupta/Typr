79e15dde2fcc448b622f893b1d4eb06ef56cf3a5
Vinit Gupta <vinitman35@gmail.com>
2021-01-05 12:06:31 +0530

    Added timer functionality with start, pause and resume options. The paragraph only changes on start and not on resume.

--- index.js
+++ index.js
@@ -5,19 +5,56 @@ let content = [
 
 let para = document.querySelector("#content-to-type");
 let start = document.querySelector("#start");
+let minutes = document.querySelector("#minutes");
+let seconds = document.querySelector("#seconds");
 
 start.addEventListener("click", loadContent);
 
 function loadContent(){
     if(start.innerHTML==="Start"){
-        start.innerHTML = "Pause";
         let paraNumber = randomParaGenerator();
         para.innerHTML = content[paraNumber];
+        startClock(seconds.innerHTML,minutes.innerHTML,start.innerHTML);//start timer
+        start.innerHTML = "Pause";
+    }
+    else if (start.innerHTML==="Resume"){
+        startClock(seconds.innerHTML,minutes.innerHTML,start.innerHTML);//resume timer
+        start.innerHTML = "Pause";
     }
     else if (start.innerHTML === "Pause"){
-        start.innerHTML="Start";
+        pauseClock(start.innerHTML); //pausing the clock
+        start.innerHTML="Resume";
     }
 }
 function randomParaGenerator(){
     return Math.floor(Math.random()*2);
+}
+function startClock(secs,mins,condition){
+    let secsVal = parseInt(secs);
+    let minsVal = parseInt(mins);
+    //After every second, check the value of condition
+    //If it is start, call the increase seconds function
+    //IF not, do nothing
+
+
+    //using window object because using local variables result in error in pausing the clock because of scoping
+     window.timerClockID = setInterval(() => { 
+        if(condition==="Start"|| condition==="Resume"){
+            secsVal = increaseSeconds(secsVal);
+            if(secsVal>59){
+                secsVal = 0;
+                minsVal=minsVal+1;
+            }
+            seconds.innerHTML = secsVal;
+            minutes.innerHTML = minsVal;
+        }
+    }, 1000);
+}   
+function increaseSeconds(secondsValue){
+    return secondsValue+1;
+}
+function pauseClock(condition){
+    if(condition==="Pause"){
+        clearInterval(window.timerClockID)
+    }
 }
