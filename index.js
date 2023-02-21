5259e740c7a952855610246eee7f9c90fcbbd887
Vinit Gupta <vinitman35@gmail.com>
2023-02-21 10:44:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
 let content = [];
 window.addEventListener('load', getData, false )
 function getData(){
-        const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '78b11c6244msh2a33837ddd5d24bp19091cjsn17744a9e286c',
-		'X-RapidAPI-Host': 'theysaidso.p.rapidapi.com'
-	}
-        };
-
-fetch('https://theysaidso.p.rapidapi.com/quote/random?language=en', options)
-	.then(response => response.json())
-	.then(response => {content = [response.contents.quote]})
-	.catch(err => {
-                console.error(err)
-                content =  [["Cosmology deals with the world as the totality of space, time and all phenomena. Historically, it has had quite a broad scope, and in many cases was founded in religion. In modern use metaphysical cosmology addresses questions about the Universe which are beyond the scope of science."]];        
-        });
-//     let page = randomParaGenerator(7268);
-//     fetch("https://quote-garden.herokuapp.com/api/v3/quotes?page="+page).then((res)=>{
-//         return res.json();
-//     }).then((data)=>{
-//         console.log(data)
-//         content = [""]
-//         data.data.forEach((obj,index)=>{
-//             if(index<5)
-//             content[0] = content[0]+obj.quoteText;
-//         })
-//         console.log(content)
-//     }).catch((err)=>{
-//     })
+    let minLength = 100;
+    fetch("https://api.quotable.io/random?minLength="+minLength).then((res)=>{
+        return res.json();
+    }).then((data)=>{
+        console.log(data)
+        content = [data.content];
+        console.log(content)
+    }).catch((err)=>{
+	    console.log(err);
+	    content =  [["Cosmology deals with the world as the totality of space, time and all phenomena. Historically, it has had quite a broad scope, and in many cases was founded in religion. In modern use metaphysical cosmology addresses questions about the Universe which are beyond the scope of science."]];
+    })
 }
  
 
